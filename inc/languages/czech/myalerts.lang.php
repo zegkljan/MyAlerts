d9f5a0b5a2de6856f80044744886bdb60fe25a27
Jan Žegklitz <zegkljan@gmail.com>
2015-12-04 08:22:02 +0100

    Added Czech translation (except ACP).

---
+++ inc/languages/czech/myalerts.lang.php
@@ -0,0 +1,48 @@
+<?php
+$l['myalerts'] = 'MyAlerts';
+
+$l['myalerts_page_title'] = 'Nedávná upozornění';
+$l['myalerts_page_getnew'] = 'Zkontrolovat nová upozornění';
+
+$l['myalerts_alerts'] = 'Upozornění';
+
+$l['myalerts_settings_page_title'] = 'Nastvení upozornění';
+
+$l['myalerts_online_location_listing'] = 'Zobrazení upozornění';
+
+$l['myalerts_unread_title'] = 'Nepřečtená upozornění';
+
+$l['myalerts_no_alerts'] = 'Žádná upozornění ke zobrazení';
+$l['myalerts_rep'] = '{1} upravil/a tvou reputaci.';
+$l['myalerts_pm'] = '{1} ti poslal/a soukromou zprávu s názvem "{2}".';
+$l['myalerts_buddylist'] = '{1} si tě přidal/a do seznamu přátel.';
+$l['myalerts_quoted'] = '{1} tě citoval/a v {2}.';
+$l['myalerts_post_threadauthor'] = '{1} odpověděl/a na tvé vlákno "{2}". Po tomto příspěvku mohou být další.';
+$l['myalerts_subscribed_thread'] = '{1} odpověděl/a na tebou odebírané vlákno "{2}".';
+
+$l['myalerts_setting_rep'] = 'Dostávat upozornění na reputaci?';
+$l['myalerts_setting_pm'] = 'Dostávat upozornění na Soukromé zprávy (SZ)?';
+$l['myalerts_setting_buddylist'] = 'Dostávat upozornění, když si tě někdo přidá do seznamu přátel?';
+$l['myalerts_setting_quoted'] = 'Dostávat upozornění, když tě někdo cituje v příspěvku?';
+$l['myalerts_setting_post_threadauthor'] = 'Dostávat upozornění, když někdo odpoví na tvé vlákno?';
+$l['myalerts_setting_subscribed_thread'] = 'Dostávat upozornění, když někdo odpoví na tebou odebírané vlákno?';
+$l['myalerts_settings_save'] = 'Uložit nastavení';
+$l['myalerts_settings_updated'] = 'Nastavení úspěšně aktualizována. Přesměrovávám zpět na stránku nastavení.';
+$l['myalerts_settings_updated_title'] = 'Nastavení upozornění aktualizována.';
+
+$l['myalerts_delete_deleted'] = 'Upozornění uspěšně smazáno.';
+$l['myalerts_delete_error'] = 'Upozornění nyní nemůže být smazáno.';
+$l['myalerts_delete_read_confirm'] = 'Opravdu smazat všechna přečtená upozornění? Toto nelze vrátit!';
+$l['myalerts_delete_all_confirm'] = 'Opravdu smazat všechna upozornění? Toto nelze vrátit!';
+$l['myalerts_delete_mass_deleted'] = 'Upozornění smazána';
+$l['myalerts_delete_all'] = 'Všechna upozornění úspěšně smazána.';
+
+$l['myalerts_usercp_nav'] = 'Upozornění';
+$l['myalerts_usercp_nav_alerts'] = 'Zobrazit upozornění';
+$l['myalerts_usercp_nav_settings'] = 'Nastavení upozornění';
+$l['myalerts_usercp_nav_delete_read'] = 'Smazat stará upozornění';
+$l['myalerts_usercp_nav_delete_all'] = 'Smazat všechna upozornění';
+
+$l['myalerts_error_alert_not_found'] = 'Upozornění nenalezeno.';
+
+$l['myalerts_delete'] = 'Smazat';
